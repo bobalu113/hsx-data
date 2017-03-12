cd84c98daffa5abcf0868015b66b9572ba4535cb
Bobby Schaetzle <bschaetzle@chegg.com>
2017-03-12 06:22:46 -0700

    styling

--- src/js/views/FilterView.js
+++ src/js/views/FilterView.js
@@ -7,7 +7,9 @@ import {Panel,
         FormGroup, 
         ControlLabel, 
         FormControl, 
-        Button} from 'react-bootstrap';
+        Button,
+        Checkbox,
+        InputGroup} from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
 import moment from 'moment';
@@ -26,13 +28,18 @@ class FilterView extends React.Component {
     }
   }
 
+  onWatchedChange(watched) {
+    this.props.onFilter(this.props.filter.set('watched', watched));
+  }
+
+  onReload() {
+    this.props.onFilter(this.props.filter);
+  }
+
   render() {
     let start = moment(this.props.filter.get('startDate'));
     let end = moment(this.props.filter.get('endDate'));
-    let label = start.format('YYYY-MM-DD'); + ' to ' + end.format('YYYY-MM-DD');;
-    if (start === end) {
-      label = start;
-    }
+    let label = start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD');
     let ranges = {
       'Today' : [ moment(), moment() ],
       'Next 7 Days' : [ moment(), moment().add(6, 'days') ],
@@ -42,35 +49,52 @@ class FilterView extends React.Component {
     }
     let onCategoryChange = this.onCategoryChange.bind(this);
     let onLockInEvent = this.onLockInEvent.bind(this);
+    let onWatchedChange = this.onWatchedChange.bind(this);
+    let onReload = this.onReload.bind(this);
     return (
       <Panel bsStyle="primary">
         <Form inline>
-          <FormGroup controlId="category">
-            <ControlLabel>Category</ControlLabel>
-            <FormControl componentClass="select" 
-                         placeholder={this.props.filter.category} 
-                         onChange={onCategoryChange}>
-              <option value="">All</option>
-              <option value={Categories.Blockbuster}>Blockbuster</option>
-              <option value={Categories.Arthouse}>Arthouse</option>
-            </FormControl>
+          <FormGroup controlId="category" id="categoryFilter">
+            <InputGroup>
+              <InputGroup.Addon><ControlLabel>Category</ControlLabel></InputGroup.Addon>
+              <FormControl componentClass="select" 
+                           placeholder={this.props.filter.get('category')} 
+                           onChange={onCategoryChange}>
+                <option value="">All</option>
+                <option value={Categories.Blockbuster}>Blockbuster</option>
+                <option value={Categories.Arthouse}>Arthouse</option>
+              </FormControl>
+            </InputGroup>
           </FormGroup>
-          <FormGroup controlId="lockIn">
-            <ControlLabel>Lock&#45;in</ControlLabel>
-            <DateRangePicker startDate={start} 
-                             endDate={end} 
-                             ranges={ranges} 
-                             onEvent={onLockInEvent}>
-              <Button>
-                <div className="pull-left"><FontAwesome name="calendar" /></div>
-                <div className="pull-right">
-                  <span>{label}</span>
-                  <span className="caret"></span>
-                </div>
-              </Button>
-            </DateRangePicker>
+          <FormGroup controlId="lockIn" id="lockInFilter">
+            <InputGroup>
+              <InputGroup.Addon><ControlLabel>Lock&#45;in</ControlLabel></InputGroup.Addon>          
+              <DateRangePicker startDate={start} 
+                               endDate={end} 
+                               ranges={ranges} 
+                               onEvent={onLockInEvent}>
+                <Button>
+                  <div className="pull-left"><FontAwesome name="calendar" /></div>
+                  <div className="pull-right">
+                    <span>{label}</span>
+                    <span className="caret"></span>
+                  </div>
+                </Button>
+              </DateRangePicker>
+            </InputGroup>
           </FormGroup>
-          <FormGroup>
+          <FormGroup controlId="watched" id="watchedFilter">
+            <Button onClick={() => { onWatchedChange(!this.props.filter.get('watched')) }}>
+              <Checkbox checked={this.props.filter.get('watched')}
+                        onClick={value => { onWatchedChange(value.target.value == 'on') }}>
+              </Checkbox>
+              <ControlLabel>Watched</ControlLabel>
+            </Button>
+          </FormGroup>
+          <FormGroup controlId="reload" id="filterReload">
+            <Button onClick={onReload}>
+              <ControlLabel>Reload</ControlLabel>
+            </Button>
           </FormGroup>
         </Form>
       </Panel>
